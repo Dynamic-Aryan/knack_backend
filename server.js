bec713b9d737cbcc8070c6b84c6443813b6279a8
aryanpachchigar <aryanpachchigar12@gmail.com>
2024-05-25 22:39:54 +0530

    knack initial

--- server.js
+++ server.js
@@ -4,8 +4,7 @@ import cloudinary from "cloudinary";
 import RazorPay from "razorpay";
 import nodeCron from "node-cron";
 import { Stats } from "./models/Stats.js";
-import dotenv from 'dotenv';
-dotenv.config();
+
 connectDB();
 
 cloudinary.v2.config({
@@ -13,10 +12,6 @@ cloudinary.v2.config({
   api_key: process.env.CLOUDINARY_CLIENT_API,
   api_secret: process.env.CLOUDINARY_CLIENT_SECRET,
 });
-
-if (!process.env.RAZORPAY_API_KEY || !process.env.RAZORPAY_API_SECRET) {
-  throw new Error('RAZORPAY_API_KEY and RAZORPAY_API_SECRET must be set');
-}
 
 export const instance = new RazorPay({
   key_id: process.env.RAZORPAY_API_KEY,
@@ -32,7 +27,6 @@ nodeCron.schedule("0 0 0 1 * *", async () => {
 });
 
 
-const port = process.env.PORT || 3000;
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is working on port:${process.env.PORT} `);
